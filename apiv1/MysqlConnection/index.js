9ef9fe824d0fd7cc6fdb06216fcf0e9d46de5ee1
thang <thangvnnc@gmail.com>
2018-11-06 20:36:20 +0700

    Change connect

--- apiv1/MysqlConnection/index.js
+++ apiv1/MysqlConnection/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host     : '103.7.40.108',
     user     : 'drugpital',
-    password : 'xxxxxxx',
+    password : 'ktteam22101509',
     database : 'zadmin_drug'
 });
 
